a55228e9634f3ef07e683245a2e05b2411a94b46
ClaudioReina <124903804+ClaudioReina@users.noreply.github.com>
2023-03-23 15:19:47 +0100

    seeder aggiornato

--- database/migrations/2023_03_22_224046_add_column_user_id_to_categories_table.php
+++ database/migrations/2023_03_22_224046_add_column_user_id_to_categories_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('categories', function (Blueprint $table) {
-            $table->unsignedBigInteger('user_id')->after('name')->default(1);
+            $table->unsignedBigInteger('user_id')->after('name')->nullable();
             $table->foreign('user_id')->references('id')->on('users');
         });
     }
