58cca79e638f36018a097f622915a3dba30116fc
DavideCariola <124775116+DavideCariola@users.noreply.github.com>
2023-03-24 12:43:44 +0100

    update AppServiceProvider boot function

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        View::share('categories', Category::All());
+        if(\Illuminate\Support\Facades\Schema::hasTable('categories'){
+            View::share('categories', Category::All());
+        }
     }
 }
