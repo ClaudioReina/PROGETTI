6a250529771aa262e5721fe848a751c5a7e8fcd9
DavideCariola <124775116+DavideCariola@users.noreply.github.com>
2023-03-24 12:44:24 +0100

    fix in boot AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if(\Illuminate\Support\Facades\Schema::hasTable('categories'){
+        if(\Illuminate\Support\Facades\Schema::hasTable('categories')){
             View::share('categories', Category::All());
         }
     }
