87ed1d6eaa1a658c56b3625d0dfd8cdb093e7b28
Melo La Mantia <carmelo.la.mantia00@gmail.com>
2023-03-27 00:59:39 +0200

    sistemato design form registrazione

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,78 +10,41 @@
   </div>
 @endif
 
-    <section>
-        <div class="container py-5 ">
-            <div class="row d-flex justify-content-center align-items-center ">
-                <div class="col-xl-10 ">
-                    <div class="card rounded-3 text-black shadow">
-                        <div class="row g-0">
-                            <div class="col-lg-6">
-                                <div class="card-body p-md-5 mx-md-4">
-
-                                    <div class="text-center mb-5">
-                                        <img src="/media/presto.it__1_-removebg-preview.png" style="width: 100px;" alt="logo">
-                                        <h1>Modulo di Registrazione</h1>
-                                    </div>
-
-                                    <form action="{{ route('register') }}" method="POST">
-                                        @csrf
-
-
-                                        <div class="form-outline mb-4">
-                                            <label class="form-label" for="form2Example11">Email</label>
-                                            <input type="email" id="email" name="email" class="form-control"
-                                                placeholder="Indirizzo Email" />
-                                        </div>
-
-                                        <div class="form-outline mb-4">
-                                            <label class="form-label" for="form2Example11">Nome Utente </label>
-                                            <input type="text" id="name" name="name" class="form-control"
-                                                placeholder="Nome utente" />
-                                        </div>
-
-                                        <div class="form-outline mb-4">
-                                            <label class="form-label" for="form2Example22">Password</label>
-                                            <input type="password" id="password" name="password"
-                                                class="form-control" />
-                                        </div>
-
-                                        <div class="form-outline mb-4">
-                                            <label class="form-label" for="form2Example22">Conferma Password</label>
-                                            <input type="password" name="password_confirmation"
-                                                id="password_confirmation" class="form-control" />
-                                        </div>
-
-                                        <div class="text-center pt-1 mb-5 pb-1">
-                                            <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3"
-                                                type="submit">Registrati
-                                            </button>
-                                        </div>
-
-                                        <div class="d-flex align-items-center justify-content-center pb-4">
-                                            <p class="mb-0 me-2">Hai già un profilo?</p>
-                                            <a href="{{route('login')}}"><button type="button"
-                                                class="btn btn-outline-danger">Accedi!</button></a>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                            <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
-                                <div class="text-white px-3 py-4 p-md-5 mx-md-4">
-                                    <h4 class="mb-4">We are more than just a company</h4>
-                                    <p class="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-                                        do eiusmod
-                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                        quis nostrud
-                                        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+<div class="container-fluid">
+    <div class="row justify-content-center">
+        <div class="col-12 col-md-6 mt-4">
+            <form class="form formLogin p-4" method="POST" action="{{route('register')}}">
+                @csrf
+                <p id="heading">Registrati</p>
+                <div class="field">
+                    <label for="name">Nome Completo</label>
+                    <input type="name" name="name" id="name" class="form-control input-field" placeholder="Indirizzo email" />
                 </div>
-            </div>
+                <div class="field">
+                    <label for="email">Email</label>
+                    <input type="email" placeholder="mario@rossi.it" name="email" id="email" class="form-control input-field" />
+                </div>
+                <div class="field">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password" class="form-control input-field" />
+                </div>
+                <div class="field">
+                    <label for="password">Conferma Password</label>
+                    <input type="password" name="password_confirmation" id="password" class="form-control input-field" />
+                </div>
+                <div class="btnLogin">
+                    <button type="submit" class="buttonLogin1 text-center">
+                        <span class="mx-3">Registrati</span>
+                    </button>
+                    <a href="{{route('login')}}">
+                        <button type="button" class="buttonLogin2">Accedi!</button>
+                    </a>
+                </div>
+                {{-- <button class="buttonLogin3">Password Dimenticata?</button> --}}
+            </form>            
         </div>
-    </section>
+    </div>
+</div>
 
     <div class="container-fluid spaced">
         
