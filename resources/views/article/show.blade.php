8fa6cc6fe7fd79ab7166f916fdf65ec4efcb6b73
francescoSvara <francesco.svara@gmail.com>
2023-04-01 11:33:22 +0200

    vari fix

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -2,26 +2,27 @@
     
     <div class="container-fluid px-5">
         <div class="row py-5 mt-5">
-            
-            <div class="col-12 col-lg-3 centerMod">  
-            <div class="carousel slide" data-bs-ride="carousel">
-                        <div class="carousel-inner">
+            <div class="col-12 col-lg-4 centerMod mx-auto text-center">  
+                <div class="carousel slide" data-bs-ride="carousel">
+                    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner d-flex align-items-center">
                             @foreach ($article->image as $key => $image)
                                 <div class="carousel-item {{ $key === 0 ? 'active' : '' }}">
-                                    <img src="{{!$article->image()->get()->isEmpty() ? $article->image()->first()->getUrl(500, 500) : '/media/ImmagineSalvaposto.jpg'}}" class="card-img-top img-custom" alt="...">
+                                    <img src="{{!$article->image()->get()->isEmpty() ? $image->getUrl(500, 500) : '/media/ImmagineSalvaposto.jpg'}}" class="card-img-top img-custom rounded-5" alt="...">
                                 </div>
                             @endforeach
                         </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target=".carousel" data-bs-slide="prev">
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                             <span class="visually-hidden">Previous</span>
                         </button>
-                        <button class="carousel-control-next" type="button" data-bs-target=".carousel" data-bs-slide="next">
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
                             <span class="carousel-control-next-icon" aria-hidden="true"></span>
                             <span class="visually-hidden">Next</span>
                         </button>
+                        </div>
                     </div>
-            </div>
+                </div>
             <div class="col-12 col-lg-6  mt-5">   
                 <h5 class="display-4 robotoFont">{{$article->title}}</h5>             
                 <p class="display-2">{{$article->price}} €</p>
@@ -29,7 +30,7 @@
             </div>
             
         </div>            
-            <div class="col-4 col-md-2 ">
+            <div class="col-4 col-md-2 pt-5 mt-5">
                 <a href="{{route('article.index')}}" class="btn btn-contact2">{{__('ui.goBack')}}</a>
             </div>
         </div>
--- resources/views/livewire/article-list.blade.php
+++ resources/views/livewire/article-list.blade.php
@@ -4,9 +4,10 @@
             <tr>
                 <th scope="col">ID</th>
                 <th scope="col">{{__('ui.title')}}</th>
+                <th scope="col">{{__('ui.publishedBy')}}</th>
                 <th scope="col">{{__('ui.category')}}</th>
                 <th scope="col">{{__('ui.price')}}</th>
-                <th scope="col">{{__('ui.createdAt')}}</th>
+                <th scope="col">{{__('ui.published')}}</th>
                 <th scope="col">{{__('ui.adsStatus')}}</th>
                 <th scope="col">{{__('ui.undoRevision')}}</th>
             </tr>
@@ -16,16 +17,17 @@
             <tr>
                 <th scope="row">{{$article->id}}</th>
                 <td>{{$article->title}}</td>
+                <td>{{$article->user->name}}</td>
                 <td>{{$article->category}}</td>
                 <td>{{$article->price}} €</td>
                 <td>{{$article->created_at}}</td>
                 <td>
                     @if($article->is_accepted == 1)
-                    <span class="label text-success">{{__('ui.accepted')}}</span>
+                        <span class="label text-success">{{__('ui.accepted')}}</span>
                     @elseif ($article->is_accepted === 0)
-                    <span class="label text-danger">{{__('ui.refused')}}</span>
+                        <span class="label text-danger">{{__('ui.refused')}}</span>
                     @else
-                    <span class="label">{{__('ui.inProgress')}}</span>
+                        <span class="label">{{__('ui.inProgress')}}</span>
                     @endif
                 </td>
                 <td>
@@ -43,4 +45,23 @@
             @endforeach
         </tbody>
     </table>
+</div>
+
+
+<div class="row justify-content-end pe-5">
+    <div class="col-1 d-flex bg-dark rounded">
+        @if ($articles->currentPage() > 1)
+            <div class="text-center text-white"><a href="{{ $articles->previousPageUrl() }}"><i class="fa fa-angle-left text-white"></i></a></div>
+        @endif
+
+        @for ($i = 1; $i <= $articles->lastPage(); $i++)
+            <div  class="text-center mx-auto text-white {{ ($i == $articles->currentPage()) ? ' active' : '' }}">
+                <a class="text-white" href="{{ $articles->url($i) }}">{{ $i }}</a>
+            </div>
+        @endfor
+
+        @if ($articles->hasMorePages())
+            <div><a href="{{ $articles->nextPageUrl() }}"><i class="fa fa-angle-right text-white"></i></a></div>
+        @endif        
+    </div>
 </div>
