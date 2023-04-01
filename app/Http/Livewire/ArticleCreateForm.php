eae2e81d2b7e89c8a7fa7f52c49aa8d9cce54699
Melo La Mantia <carmelo.la.mantia00@gmail.com>
2023-04-01 16:50:35 +0200

    aggiunto job removeFaces

--- app/Http/Livewire/ArticleCreateForm.php
+++ app/Http/Livewire/ArticleCreateForm.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Image;
 use App\Models\Article;
 use Livewire\Component;
-use App\Models\Image;
 use App\Models\Category;
+use App\Jobs\RemoveFaces;
 use App\Jobs\ResizeImage;
 use Livewire\WithFileUploads;
 use Spatie\Image\Manipulations;
@@ -96,9 +97,12 @@ class ArticleCreateForm extends Component
                 $newFileName = "article/{$this->article->id}";
                 $newImage = $this->article->image()->create(['path' => $image->store($newFileName , 'public')]);
 
-                dispatch(new ResizeImage($newImage->path, 500 , 500));
-                dispatch(new GoogleVisionSafeSearch($newImage->id));
-                dispatch(new GoogleVisionLabelImage($newImage->id));
+                RemoveFaces::withChain([
+                    new ResizeImage($newImage->path, 500 , 500),
+                    new GoogleVisionSafeSearch($newImage->id),
+                    new GoogleVisionLabelImage($newImage->id),
+                ])->dispatch($newImage->id);
+            
             }
 
             File::deleteDirectory(storage_path('/app/livewire-tmp'));
