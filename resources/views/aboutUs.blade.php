f7ff0e6769b64e9ca68fe31811f9388e86091a40
HashFresh <cook96dani@gmail.com>
2023-04-03 18:46:59 +0200

    aggiunta sezione about us

--- resources/views/aboutUs.blade.php
+++ resources/views/aboutUs.blade.php
@@ -1,5 +1,78 @@
 <x-layout>
-
     
+    <div class="container-fluid aboutUsBackground">
+        <div class="row justify-content-center">
+            <div class="col-12 mb-5 pb-5 ">
+                <h2 class="robotoFont text-center display-3 text-white hidden" >Chi siamo</h2>
+            </div>
+            
+        </div>
+        
+        <div class="row justify-content-evenly ">
 
+            <div class="col-12 col-md-3 hidden-left">
+                <div class="roundProfile mx-auto">
+                    <div class="text-white custom-p text-center">
+                        <p>Daniele Cuoco <br> <span>Front-end</span></p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-3 hidden-left">
+                <div class="roundProfile2 mx-auto">
+                    <div class="text-white custom-p text-center">
+                        <p>Nicola Rizzitelli <br> <span>Back-end</span></p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-12 col-md-3">
+                <div class="roundProfile3 mx-auto hidden-right">
+                    <div class="text-white custom-p text-center">
+                        <p>Francesco Svara <br> <span>Back-end</span></p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-3">
+                <div class="roundProfile4 mx-auto hidden-right">
+                    <div class="text-white custom-p text-center">
+                        <p>Carmelo La Mantia <br> <span>Back-end</span></p>
+                    </div>
+                </div>
+            </div>
+            
+
+            <div class="spacedSxM">
+    
+            </div>
+
+            <div class="col-12 col-md-3 mt-5 hidden-left">
+                <div class="roundProfile5 mx-auto ">
+                    <div class="text-white custom-p text-center">
+                        <p>Giovanni Botea <br> <span>Back-end</span></p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-12 col-md-3 mt-5 hidden-right">
+                <div class="roundProfile6 mx-auto  ">
+                    <div class="text-white custom-p text-center">
+                        <p>Claudio Reina <br> <span>Front-end</span></p>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+        <div class="spaced">
+    
+        </div>
+        
+    </div>
+    
+    
+    
+   
+    
+    
 </x-layout>
